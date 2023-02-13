8e0341dc53e00402c5253ff0da7452cbc896fa96
soufieddin <eddin2209@gmail.com>
2023-02-13 15:05:05 +0100

    hendled errors

--- app/root.jsx
+++ app/root.jsx
@@ -1,10 +1,12 @@
 import {
+  Link,
   Links,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useCatch,
 } from "@remix-run/react";
 import MainNavigation from "~/components/MainNavigation";
 import styles from '~/styles/main.css';
@@ -25,6 +27,57 @@ export default function App() {
       <body>
         <MainNavigation />
         <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
+
+export function CatchBoundary() {
+  const coughtResponse = useCatch();
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+        <title>{coughtResponse.statusText}</title>
+      </head>
+      <header>
+        <MainNavigation />
+      </header>
+      <body>
+        <main className="error">
+          <h1>{coughtResponse.statusText}</h1>
+          <p>{coughtResponse.data?.message || "Something went wrong!"}</p>
+          <p>Back to <Link to="/">safty</Link></p>
+        </main>
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
+
+export function ErrorBoundary({error}) {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+        <title>An error occurred! </title>
+      </head>
+      <header>
+        <MainNavigation />
+      </header>
+      <body>
+        <main className="error">
+          <h1>An error occurred!</h1>
+          <p>{error.message}</p>
+          <p>Back to <Link to="/">safty</Link></p>
+        </main>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -1,5 +1,6 @@
 
-import { useLoaderData } from "@remix-run/react";
+import { Link, useCatch, useLoaderData } from "@remix-run/react";
+import { json } from "@remix-run/node";
 import NoteList, {links as noteListLinks} from "~/components/NoteList";
 import { getStoredNotes } from "~/data/notes";
 export default function NotesPage() {
@@ -14,9 +15,23 @@ export default function NotesPage() {
 //backend (fetch notes)
 export async function loader() {
   const notes = await getStoredNotes();
+  if(!notes || notes.length === 0) {
+    throw json({message:"Could not find any notes"},{
+      status: 404,  
+      statusText: "Not Found",
+    });
+  }
   return notes;
 }
 
+export function CatchBoundary() {
+  const coughtResponse = useCatch();
+  const msg = coughtResponse.data.message || "Data not found";
+
+  return <main>
+    <p className="info-message">{msg}<br /><Link className="link" to="/add">Add a note</Link></p>
+  </main>
+}
 export function links() {
   return [...noteListLinks()];
 }
