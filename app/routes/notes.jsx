998b9f8395536f3e8407a0f5b0d4fbc7f22fcfb0
soufieddin <eddin2209@gmail.com>
2023-02-13 12:01:58 +0100

    Added some backend

--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -1,4 +1,7 @@
+import { redirect } from '@remix-run/node';
+import { v4 as uuidv4 } from 'uuid';
 import NewNote, {links as NewNoteLinks} from "~/components/NewNote";
+import { getStoredNotes, storeNotes } from "~/data/notes";
 
 export default function NotesPage() {
   return (
@@ -6,6 +9,17 @@ export default function NotesPage() {
       <NewNote />
     </main>
   );
+}
+
+//backend
+export async function action({request}) {
+    const formData = await request.formData();
+    const noteData =Object.fromEntries(formData);
+    const existingNotes = await getStoredNotes();
+    noteData.id = uuidv4();
+    const updatedNotes = existingNotes.concat(noteData);
+    await storeNotes(updatedNotes);
+    return redirect("/notes")
 }
 
 export function links() {
